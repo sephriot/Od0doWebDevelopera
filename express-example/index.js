a0315011e4cf089563f21884c8e29ab30ac77fcb
Jakub Wójtowicz <wojtjk@gmail.com>
2022-01-04 00:26:04 +0100

    mysql 3/4

--- express-example/index.js
+++ express-example/index.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
     password: 'sephcode'
 })
 
-app.get('/', (req, response) => {
+app.get('/list', (req, response) => {
 
     db.query('SELECT * FROM `users`', (err, res) => {
         if(err) {
@@ -20,6 +20,32 @@ app.get('/', (req, response) => {
         }
         response.send(res)
     })
+});
+
+app.get('/', (req, response) => {
+
+    db.query("INSERT INTO `users` SET name=?;", [req.query.name],
+        (err, res) => {
+        if(err) {
+            console.error(err)
+            throw err
+        }
+        response.send({"status": "OK"})
+    })
+});
+
+app.delete('/', (req, response) => {
+
+    console.log("ID:" + req.query.id)
+    db.query("DELETE FROM `users` WHERE id=?;", [req.query.id],
+        (err, res) => {
+            console.log(res)
+            if(err) {
+                console.error(err)
+                throw err
+            }
+            response.send({"status": "DELETED OK"})
+        })
 });
 
 app.listen(port, () => {
